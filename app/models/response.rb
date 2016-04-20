3b2808b0d28dc0cefcd1cdd248dfbc508e3b29c1
Emmett Garber & Mike Verthein <pair+emmettgarber.mverthein@devbootcamp.com>
2016-04-20 13:56:00 -0500

    seeds

--- app/models/response.rb
+++ app/models/response.rb
@@ -2,5 +2,5 @@ class Response < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :user
   belongs_to :respondable, polymorphic: true
-  has_many :votes
+  has_many :votes, as: :votable
 end
