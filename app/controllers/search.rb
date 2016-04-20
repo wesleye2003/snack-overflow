f1c4220ee9d89705dc83881c7389141f7c3f16b3
Emmett Garber & Mike Verthein <pair+emmettgarber.mverthein@devbootcamp.com>
2016-04-20 18:06:07 -0500

    This search bar is functioning and fully operation, your rebel friends stand no chance

---
+++ app/controllers/search.rb
@@ -0,0 +1,17 @@
+post '/search'  do
+  @search_item = params[:query]
+  @questions = []
+  tag = Tag.find_by(tag_name: @search_item)
+  q_by_title_body = Question.where("title LIKE '%#{@search_item}%' OR body LIKE '%#{@search_item}%'")
+  if tag != nil && q_by_title_body != nil
+    tag.questions.each {|item| @questions << item}
+    q_by_title_body.each {|item| @questions << item}
+  elsif tag != nil
+    @questions = tag.questions
+  elsif q_by_title_body != nil
+      @questions = q_by_title_body
+  else
+    @questions = []
+  end
+  erb :'search/results'
+end
