bfc03866c2357d5f64138e7e836b5349cb00314d
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2016-04-20 11:25:53 -0500

    add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
+  
+  has_many :questions
+  has_many :answers
+  has_many :votes
+  has_many :responses
+
+
   validates :username, uniqueness: true, presence: true
   validates :password_digest, presence: true
 end
