d5497af3e2f85823becab9d5774bf13838c80029
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2016-04-20 11:17:11 -0500

    add associations, add point_total method

--- db/migrate/20160420103057_create_answers.rb
+++ db/migrate/20160420103057_create_answers.rb
@@ -3,6 +3,8 @@ class CreateAnswers < ActiveRecord::Migration
   	create_table :answers do |t|
   		t.string   :body
   		t.integer  :user_id, :question_id
+
+  		t.timestamps null: false
   	end
   end
 end
