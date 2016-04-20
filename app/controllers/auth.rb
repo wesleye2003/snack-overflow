3e37a8d7d89cac0b609f48f8825e16ba0f79f69f
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2016-04-20 14:04:11 -0500

    add working login, logout and create features

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -3,13 +3,25 @@ get '/' do
 end
 
 get '/login' do
-  
+  erb :login
 end
 
-get '/signup' do
-  
+post '/login' do
+	user = User.find_by(username: params[:username])
+	if user.authenticate
+		session[:user_id] = user.id
+		redirect "/"
+	else
+		@errors = ["Password/email combination is incorrect"]
+		erb :login
+	end
+end
+
+get '/users/new' do
+  erb :'users/new'
 end
 
 get '/logout' do
-	session[:user_id].delete
+	session.delete(:user_id)
+	redirect '/'
 end
