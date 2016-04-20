a9d25300304472dda97302187a31fd991e2d4f6e
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2016-04-20 11:21:02 -0500

    add timestamps, add associations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,11 @@
 class Question < ActiveRecord::Base
-  validates :title, :body, :user_id, presence: true
+	belongs_to :user
+	has_many :votes, as: :votable
+	has_many :responses, as: :respondable
+	has_many :taggings
+	has_many :tags, through: :taggings
+	has_many :answers
 
+  validates :title, :body, :user_id, presence: true
+  ###########METHOD TO DETERMINE PRIMARY TAGGING?################ 
 end
--- db/migrate/20160420103104_create_questions.rb
+++ db/migrate/20160420103104_create_questions.rb
@@ -3,6 +3,8 @@ class CreateQuestions < ActiveRecord::Migration
   	create_table :questions do |t|
   		t.string   :title, :body
   		t.integer  :user_id
+
+  		t.timestamps null: false
   	end
   end
 end
